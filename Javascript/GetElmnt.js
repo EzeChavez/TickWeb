a72e5e914bd7956948968303fc8daf713972e10f
Gamble Black <eze6532@gmail.com>
2022-09-09 16:59:30 +0200

    Agregue JS para la imagen del index

--- Javascript/GetElmnt.js
+++ Javascript/GetElmnt.js
@@ -8,7 +8,7 @@ document.getElementById("foto").src="imgs/Tick.ar2.png";
 function  cambia2()
 {
 
-document.getElementById("foto").src="imgs/Tick.ar3.png";
+document.getElementById("foto").src="imgs/Tick.ar.png";
 
 }
 
